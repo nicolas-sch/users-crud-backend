d42984539a3c864ed9769baa47cd6ef35cd2781e
nicolas_schmidt <nicolasschmidt@deltaglobal.com.br>
2022-08-07 10:48:08 -0300

    user update created

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ app.post('/users', (request, response) => {
     return response.status(201).json(user)
 })
 
+app.put('/users/:id', (request, response) => {
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updateUser = { id, name, age }
+    
+    const index = users.findIndex( user => user.id === id)
+
+    if(index < 0) {
+        return response.status(404).json({ message: "User Not Found"})
+    }
+
+    users[index] = updateUser
+
+    return response.json(updateUser)
+})
+
+
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000🚀')
 })
